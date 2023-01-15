7a03f705f1a0df6447b4d06aaf6995d77501709d
nadav <nadavlevi99@gamil.com>
2023-01-15 13:50:40 +0200

    start2

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(":D")
+    # please
 
 
 # Press the green button in the gutter to run the script.
